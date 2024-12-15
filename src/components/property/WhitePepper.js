4afda3265eb745a950fd827aeaf4fa1028bf700c
spideh gooshei <sgooshei@gmail.com>
2024-12-15 11:04:31 +0330

    del-image

--- src/components/property/WhitePepper.js
+++ src/components/property/WhitePepper.js
@@ -30,34 +30,3 @@ export default function WhitePepper() {
   
 
 
-
-/* export default function WhitePepper() {
-    return (
-        <div className="card shadow p-3 mb-5 bg-body rounded">
-            <img
-                src="../../assets/skin/White pepper.jpg"
-                alt="White pepper"
-                className="img-fluid"
-            />
-            <h6 className="m-3 text-end fw-bold"> فلفل سفید </h6>
-            
-            <div className="card-body text-end">
-                <div className="card-title"> خواص فلفل سفید </div>
-                <div className="card-text">
-                    <p className="lh-lg px-2">
-                    فلفل سفید یکی از محبوب‌ترین ادویه‌ها است که طعم و عطر بسیار خوبی دارد. از فلفل سفید در تهیه سوپ‌ها، غذاهای سبک، سس‌ها، سفره آرایی، تاس کباب، آبگوشت و به عنوان چاشنی استفاده می‌شود. اما فلفل سفید علاوه بر کاربرد در آشپزی خواص و فواید درمانی بسیاری دارد که آن را به یک گیاه دارویی مهم تبدیل کرده است 
-                    </p>
-
-                <div className="card-title">فلفل سفید و پوست </div>
-
-                <div className="card-text">
-                    <p className="lh-lg">
-                    فلفل سفید برای درمان برخی از مشکلات پوستی مفید و تاثیرگذار است. فلفل سفید حاوی ماده پیپرین است که پیپرین عملکرد‌های فراوانی دارد که ازجمله مهم‌ترین عملکرد‌های این ماده می‌توان به افزایش ظرفیت مواد مغذی مانند بتا کاروتن، کورکومین، سلنیوم، ویتامین B6 و آمینو اسید اشاره کرد
-                     </p>
-                </div>              
-                  </div>
-            </div>
-        </div>
-    );
-}
- */
